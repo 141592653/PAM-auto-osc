4ba402a5db7df3bf55937455c35b4015b080c72f
GOLVET Théo <tgolvet6@gmail.com>
2020-01-21 13:54:54 +0100

    Modele modal

---
+++ Matlab_modele_modal/clarinet_modal.m
@@ -0,0 +1,36 @@
+T = 2;
+dt = 0.0001;
+
+N = floor(T/dt);
+
+p = zeros(1,N);
+u = zeros(1,N);
+
+c = 340;
+
+
+for i = 2:N
+    L = 1;
+    t = i*dt;
+    gamma = t/T*0.6 + (1-t/T)*0.8;
+    zeta = .9;
+    F1 = 2*c/L;
+    f = c/4/L;
+    w1 = 2*pi*f;
+    Q1 = 10;
+    Y1 = w1/Q1/F1;
+    A = zeta*(3*gamma-1)/2/sqrt(gamma);
+    B = -zeta*(3*gamma+1)/8/gamma^(3/2);
+    C = -zeta*(gamma+1)/16/gamma^(5/2);
+    F0 = zeta*(1-gamma)*sqrt(gamma);
+    s1 = w1/2/Q1*(-1 + 1i * sqrt(4*Q1^2 - 1));
+    C1 = F1 *(1+1i/sqrt(4*Q1^2 - 1));
+    pr = 2*real(p(i-1));
+    u(i) = F0 + A*pr + B*pr^2 + C*pr^3;
+    p(i) = (p(i-1) + dt*C1*u(i))/(1 - dt*s1);
+end
+
+plot(real(p))
+
+Fs = 1/dt;
+soundsc(real(p), Fs)
