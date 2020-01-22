5effa81097e15c7b2357cd60d5a38c578fff6084
ColetteVois <c.voisembert@gmail.com>
2020-01-22 11:28:58 +0100

    new non linearity

---
+++ Matlab_modele_physique/qh1.m
@@ -0,0 +1,24 @@
+function out = qh1(t, q, f, Zc, T, b, a, deltaT)
+% calcul of qh depending on former q(t), f(t)
+    N = length(q);
+    Y = zeros(N,1);
+    for k=1:length(q)
+       x=(k-1)*deltaT;
+       if(t>=x)
+           Y(k) = a*exp(-b*(t-x-T)^2)*(q(k)+Zc*f(k));
+           %a*exp(-b*(deltaT-T)^2)%*(q(2)+Zc*f(2))
+       end
+    end
+    X = linspace(0, (N-1)*deltaT, N);
+    out = trapz(X,Y);    
+end
+
+
+function out = r(t, T, b, a)
+    if(t<0) 
+        out = 0;
+    else
+        out = a*exp(-b*(t-T).^2);
+    end
+
+end
