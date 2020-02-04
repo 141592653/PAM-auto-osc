19004d7b1e02749f36f860d9c12201f4db2314b1
Theo Golvet <tgolvet6@gmail.com>
2020-02-04 16:44:27 +0100

    modele modal

--- Matlab_modele_modal/clarinet_modal2.m
+++ Matlab_modele_modal/clarinet_modal2.m
@@ -1,24 +1,37 @@
-function [p,u] = clarinet_modal2(zeta, gamma, T, dt, f, Q, Z)
+function [p,u] = clarinet_modal2(zeta, gamma, T, dt, f, Q, Z, p0, u0, reedModel)
     N = floor(T/dt);
 
     n = length(f);
     p = zeros(n,N);
     u = zeros(1,N);
     
+    if nargin>=9
+        u(1) = u0;
+    end
+    if nargin >= 8
+        p(1) = p0;
+    end    
+    
+    %Set parameters 
+    w = 2*pi*f;
+    A = zeta*(3*gamma-1)/2/sqrt(gamma);
+    B = -zeta*(3*gamma+1)/8/gamma^(3/2);
+    C = -zeta*(gamma+1)/16/gamma^(5/2);
+    F0 = zeta*(1-gamma)*sqrt(gamma);
+    s = (w/2./Q.*(-1 + 1i * sqrt(4*Q.^2 - 1)))';
+    D = (Z.*w./Q.*(1+1i./sqrt(4*Q.^2 - 1)))';
+    
     for i = 2:N
-        w = 2*pi*f;
-        A = zeta*(3*gamma-1)/2/sqrt(gamma);
-        B = -zeta*(3*gamma+1)/8/gamma^(3/2);
-        C = -zeta*(gamma+1)/16/gamma^(5/2);
-        F0 = zeta*(1-gamma)*sqrt(gamma);
-        s = (w/2./Q.*(-1 + 1i * sqrt(4*Q.^2 - 1)))';
-        D = (Z.*w./Q.*(1+1i./sqrt(4*Q.^2 - 1)))';
         pr = 2*real(sum(p(:,i-1)));
-        %u(i) = F0 + A*pr + B*pr^2 + C*pr^3;
-        if gamma - pr < 1
-            u(i) = zeta.*(1-gamma+pr).*sqrt(abs(gamma-pr)).*sign(gamma - pr) ;
+        if nargin >= 10 && reedModel == 2
+            if gamma - pr < 1
+                u(i) = zeta.*(1-gamma+pr).*sqrt(abs(gamma-pr)).*sign(gamma - pr) ;
+            else
+                u(i) = 0;
+            end
         else
-            u(i) = 0;
+            u(i) = F0 + A*pr + B*pr^2 + C*pr^3;
+            %u(i) = 1/(3-pr);
         end
         p(:,i) = (p(:,i-1) + dt*D*u(i))./(1 - dt*s);
     end
--- Matlab_modele_modal/main.m
+++ Matlab_modele_modal/main.m
@@ -3,12 +3,15 @@ H = f/f(1);
 Q = [18.6, 29, 33.5, 35.7, 38, 38, 37.7, 36.9, 34.9, 31.1, 32.3, 1.1]*2;
 Z = [58.12, 38.2, 48.22, 59.72, 62.53, 61.88, 45.12, 27.8, 14.12, 6.54, 3.39, 1.46];
 
+% f = 440;
+% Q = 40;
+% Z = 50;
 
 T = 1;
 Fs = 44100;
 N = T*Fs;
 pr = zeros(1,N);
-p = clarinet_modal2(0.3, 0.4, T, 1/Fs, f,Q,Z);
+p = clarinet_modal2(0.3, 0.2, T, 1/Fs, f,Q,Z);
 for i = 1:N
     pr(i) = sum(real(p(:,i)));
 end
@@ -16,4 +19,5 @@ end
 plot(pr)
 %figure
 %spectrogram(pr, 'yaxis')
-audiowrite('testnew.wav',pr/10, Fs)
+%soundsc(pr, Fs)
+%audiowrite('testnew.wav',pr/10, Fs)
