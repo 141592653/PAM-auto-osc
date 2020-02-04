7f1b8981495fc27df74daf3e4038963152615f53
ColetteVois <c.voisembert@gmail.com>
2020-02-04 15:08:16 +0100

    trompet excitator

--- Matlab_modele_modal/clarinet_modal2.m
+++ Matlab_modele_modal/clarinet_modal2.m
@@ -1,11 +1,14 @@
-function [p,u] = clarinet_modal2(zeta, gamma, T, dt, f, Q, Z)
+function [p,u] = clarinet_modal2(zeta, gamma, T, dt, f, Q, qr, Z)
     N = floor(T/dt);
 
     n = length(f);
     p = zeros(n,N);
     u = zeros(1,N);
+    x = zeros(1,N);
     
-    for i = 2:N
+    wr = 2*pi*f(1);
+    
+    for i = 3:N
         w = 2*pi*f;
         A = zeta*(3*gamma-1)/2/sqrt(gamma);
         B = -zeta*(3*gamma+1)/8/gamma^(3/2);
@@ -14,12 +17,23 @@ function [p,u] = clarinet_modal2(zeta, gamma, T, dt, f, Q, Z)
         s = (w/2./Q.*(-1 + 1i * sqrt(4*Q.^2 - 1)))';
         D = (Z.*w./Q.*(1+1i./sqrt(4*Q.^2 - 1)))';
         pr = 2*real(sum(p(:,i-1)));
+        
+        %clarinet reed simplified
         %u(i) = F0 + A*pr + B*pr^2 + C*pr^3;
-        if gamma - pr < 1
-            u(i) = zeta.*(1-gamma+pr).*sqrt(abs(gamma-pr)).*sign(gamma - pr) ;
-        else
-            u(i) = 0;
-        end
+        
+        %clarinet reed
+%         if gamma - pr < 1
+%             u(i) = zeta.*(1-gamma+pr).*sqrt(abs(gamma-pr)).*sign(gamma - pr) ;
+%         else
+%             u(i) = 0;
+%         end
+        %lippal reed
+        %x(i) = (x(i-1)-pr - 1/wr/wr*(x(i-2)-2*x(i-1))/dt/dt + qr/wr*x(i-1)/dt)/(1/(wr*dt)^2 + qr/(wr*dt));
+        x(i) = (-pr - 1/wr/wr*(x(i-2)-2*x(i-1))/dt/dt + qr/wr*x(i-1)/dt)/(1/(wr*dt)^2 + qr/(wr*dt)+1);
+        u(i) = zeta.*(1+gamma+x(i)).*sqrt(abs(gamma-pr)).*sign(gamma - pr) ;
+        
+        %violon excitator
+        
         p(:,i) = (p(:,i-1) + dt*D*u(i))./(1 - dt*s);
     end
 end
