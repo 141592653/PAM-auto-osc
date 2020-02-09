5f97a804df7cb2ded200f3c02c80b34759af7cb6
ColetteVois <c.voisembert@gmail.com>
2020-02-09 15:19:35 +0100

    coherance de tous les code debut

--- Matlab/Svm/svm.m
+++ Matlab/Svm/svm.m
@@ -7,8 +7,9 @@
 % args (matrice) = liste des arguments constants pour le modèle
 % path (string) = le dossier où enregistrer l'image
 
-function svm(model_name, args, desc_name, nb_points, nb_edsd, instrument_name, path, formats)
-    if(nargin > 9)
+function svm(model_name, desc_name, exc_name, resonator_name, para_fixe, args,...
+    nb_points, nb_edsd, nom_svm, path, formats)
+    if(nargin > 11)
         error("too many inputs");
     end
     
@@ -16,6 +17,11 @@ function svm(model_name, args, desc_name, nb_points, nb_edsd, instrument_name, p
     fprintf("Le modèle est %s\n", model_name);
 
     seuil = 2;
+    
+    % arguments 
+    if(strcmpi(model_name, 'clarinet_modal2'))
+        [F,Z,Q] = paraResonator(resonator_name);
+    end
     
     % chercher pointeur sur la fonction de descripteur
     if(strcmpi(desc_name, 'oscillation'))
@@ -27,19 +33,45 @@ function svm(model_name, args, desc_name, nb_points, nb_edsd, instrument_name, p
     
     % chercher pointeur sur la fonction du modèle
     if(strcmpi(model_name, 'clarinet_modal2'))
-        model = @(zeta, gamma, args) clarinet_modal2(zeta, gamma, args);
+        model = @(args) clarinet_modal2(args, para_fixe, F, Z, Q);
     elseif(strcmpi(model_name, 'clarinet_modal'))
         model = @(args) clarinet_modal(args);
     end
-
+%%
     % créer les points
     fprintf("création des points\n");
-    x = lhsdesign(nb_points,2);
-    %x(:,3) = 1000*x(:,3);
+    x = lhsdesign(nb_points,2); 
     
+    min_svm = [0,0];
+    reedmodel = 1;
+    if(strcmpi(exc_name, 'reed'))
+        reedmodel = reedmodel + 1;
+    end
     
+
+    % les parametres de la svm
+    if((strcmpi(para_fixe, 'L')))
+        % zeta gamma
+        L = 0.5; % fixe     
+        max_svm = [1,1];      
+        args0 = [L, reedmodel];
+    elseif((strcmpi(para_fixe, 'gamma')))
+        % zeta L
+        gamma = 0.8;
+        x(:,2) = 2*x(:,2); % L peut varier jusqu'à 2m
+        max_svm = [1,2]; 
+        args0 = [gamma, reedmodel];
+    else
+    % gamma L
+        zeta = 0.8;
+        x(:,2) = 2*x(:,2); % L peut varier jusqu'à 2m
+        max_svm = [1,2]; 
+        args0 = [zeta, reedmodel];
+    end
+     
     fprintf("appliquer le descripteur à tous les points\n");
     
+    args = [args0, args];
     result = @(x) descriptor(real(model(x(1), x(2), args))');
     
     y = zeros(1, nb_points);
@@ -53,16 +85,13 @@ function svm(model_name, args, desc_name, nb_points, nb_edsd, instrument_name, p
     
     % 2 cas différents : descriptor -1 ou +1
     % descriptor à plusieurs seuils
-    
-    % if(seuil == 2)
-        
     SVM = CODES.fit.svm(x,y);
-    
     fprintf("Svm fit done\n");
-   
-    svm_col = CODES.sampling.edsd(result, SVM, [0 0], [1 1] , 'iter_max', nb_edsd, 'conv', false);
     
+    svm_col = CODES.sampling.edsd(result, SVM, min_svm, max_svm, 'iter_max', nb_edsd, 'conv', false);
+
     fprintf("edsd fit done\n");
+    
     figure;
     svm_col{end}.isoplot('legend', false, 'sv', false, 'samples', false)
     %axis equal 
@@ -78,7 +107,8 @@ function svm(model_name, args, desc_name, nb_points, nb_edsd, instrument_name, p
             fprintf("seuil %d\n", i);
             SVM = CODES.fit.svm(svm_end.X, svm_end.Y + i);
             fprintf("edsd seuil %d\n", i);
-            svm_col = CODES.sampling.edsd(g, SVM, [0 0], [1 1] , 'iter_max', nb_edsd, 'conv', false);
+
+            svm_col = CODES.sampling.edsd(g, SVM, min_svm, max_svm, 'iter_max', nb_edsd, 'conv', false);
             % ?? pas sûre de s'il faut faire cette prochaine ligne 
             % ou continuer avec le svm_end du début
             svm_end = svm_col{end};
@@ -87,11 +117,33 @@ function svm(model_name, args, desc_name, nb_points, nb_edsd, instrument_name, p
         end
     end
     
+    
+    % la partie récupération des courbes ne fonctionne pas 
+    
+%     fprintf("Récupére les courbes\n");
+%     % récupérer toutes les courbes
+%     courbe = findobj(gcf,'Color','g');
+%     xd = get(courbe,'XData');
+%     yd = get(courbe,'YData');
+%     
+%     
+%     figure;
+%     plot(xd, yd);
+%     
+%     % enregistrer les graphes en images
+%     filename = strcat('svm_', instrument_name, int2str(num));
+%     
+%     fprintf("%s\n", filename);
+%     saveas(gcf, filename, format);
+%     
+%     %saveas(gcf, filename, 'm');
+
+
     % enregistrer les graphes en images
     if(~exist(path, 'dir'))
        mkdir(path); 
     end
-    filename = strcat(path, 'svm_', instrument_name);
+    filename = strcat(path, 'svm_', desc_name);
     
     for i=1:length(formats)
         saveas(gcf, filename, formats(i));
