bbf9504f42e0c12c63d99d40a7dce961909c6f2c
Theo Golvet <tgolvet6@gmail.com>
2020-02-11 16:34:30 +0100

    modes

--- Matlab/Modele_Physique/modele_modal/paraResonator.m
+++ Matlab/Modele_Physique/modele_modal/paraResonator.m
@@ -1,4 +1,4 @@
-function [f,Q,Z] = paraResonator(resonator)
+function [f,Z,Q] = paraResonator(resonator, f0)
 
     if(strcmpi(resonator, 'clarinet'))
         % 2 modes
@@ -6,13 +6,30 @@ function [f,Q,Z] = paraResonator(resonator)
         % Z = 10*[1 0.75];
         % Q = [10 15];
 
-        % 7 modes
+        c = 340;
+            
+        f_ratio = [1 3];
+        
+        kn = 2*pi*f0*f_ratio/c;
+        
+        a1 = 1.044;
+        R = 0.03;
+        rho = 1.4;
+        S = pi*R^2;
+        Zc = rho*c/S;
+        rv = 1/R * 2*(1e-4)./sqrt(kn);
+        
+
+        f = f0*f_ratio.*(1 - a1*rv);
+        Q = 1./(2*a1*rv);
+        Z = Zc*8*f0*Q./(2*pi*f)/1000000;
+        
+%         %7 modes
 %         f = 235.5*[1 3 5 7 9 11 13];
 %         Z = 10*[1 0.75 0.5 0.14 0.15 .12 .17];
 %         Q = [10 15 20 25 30 40 50];
-        f= [440 880];
-        Q = [10 15];
-        Z = [10 5];
+%         Z = [10 7.5];
+%         Q = [10 15];
     elseif(strcmpi(resonator, 'saxophone'))
         % 2 modes
         % f = 500*[1 2];
@@ -20,12 +37,27 @@ function [f,Q,Z] = paraResonator(resonator)
         % Q = [10 15];
 
         % 7 modes
-        f = 235.5*[1 2 4 6 8 10 12];
-        Z = 10*[1 0.75 0.5 0.14 0.15 .12 .17];
-        Q = [10 15 20 25 30 40 50];
+        c = 340;
+            
+        f_ratio = [1 2 3 4 5 6];
+        
+        kn = 2*pi*f0*f_ratio/c;
+        
+        a1 = 1.044;
+        R = 0.03;
+        rho = 1.4;
+        S = pi*R^2;
+        Zc = rho*c/S;
+        rv = 1/R * 2*(1e-4)./sqrt(kn);
+        
+
+        f = f0*f_ratio.*(1 - a1*rv);
+        Q = 1./(2*a1*rv);
+        Z = Zc*8*f0*Q./(2*pi*f)/1000000;
+        
     elseif(strcmpi(resonator, 'trompet'))
         f = [88.16, 237.16, 353.89, 473.8, 591.29, 706.7, 820.3, 932.05, 1054.77, 1173.35, 1284.21, 1397.63]*2;
-        H = f/f(1);
+        f = f0*f/f(1);
         Q = [18.6, 29, 33.5, 35.7, 38, 38, 37.7, 36.9, 34.9, 31.1, 32.3, 1.1]*2;
         Z = [58.12, 38.2, 48.22, 59.72, 62.53, 61.88, 45.12, 27.8, 14.12, 6.54, 3.39, 1.46];
     end
